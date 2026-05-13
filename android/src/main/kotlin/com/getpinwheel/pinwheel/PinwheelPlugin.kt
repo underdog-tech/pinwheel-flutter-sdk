00fa756b706d220d9ff8f8aa07f907e90a3680bc
Wes Garlock <wes.garlock@pinwheelapi.com>
2026-05-13 11:44:15 -0400

    Update event switch

--- android/src/main/kotlin/com/getpinwheel/pinwheel/PinwheelPlugin.kt
+++ android/src/main/kotlin/com/getpinwheel/pinwheel/PinwheelPlugin.kt
@@ -87,12 +87,137 @@ class PluginListener(messenger: BinaryMessenger) : PinwheelEventListener {
   }
 
   override fun onEvent(eventName: PinwheelEventType, payload: PinwheelEventPayload?) {
-    val argument = gson.toJson(
-      PinwheelEventChannelArgument(
-        eventName.toString().lowercase(),
-        payload?.let { gson.toJson(it) }
-      )
-    )
+    var argument: String? = null
+    when (eventName) {
+      PinwheelEventType.OPEN -> {
+        val obj = PinwheelEventChannelArgument("open", null)
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.SELECT_EMPLOYER -> {
+        val obj = PinwheelEventChannelArgument("select_employer", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.SELECT_PLATFORM -> {
+        val obj = PinwheelEventChannelArgument("select_platform", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.INCORRECT_PLATFORM_GIVEN -> {
+        val obj = PinwheelEventChannelArgument("incorrect_platform_given", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.LOGIN -> {
+        val obj = PinwheelEventChannelArgument("login", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.LOGIN_ATTEMPT -> {
+        val obj = PinwheelEventChannelArgument("login_attempt", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.INPUT_ALLOCATION -> {
+        val obj = PinwheelEventChannelArgument("input_allocation", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.INPUT_REQUIRED -> {
+        val obj = PinwheelEventChannelArgument("input_required", null)
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.EXIT -> {
+        val obj = PinwheelEventChannelArgument("exit", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.SUCCESS -> {
+        val obj = PinwheelEventChannelArgument("success", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.ERROR -> {
+        val obj = PinwheelEventChannelArgument("error", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.CARD_SWITCH_BEGIN -> {
+        val obj = PinwheelEventChannelArgument("card_switch_begin", null)
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.DD_FORM_BEGIN -> {
+        val obj = PinwheelEventChannelArgument("dd_form_begin", null)
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.DD_FORM_CREATE -> {
+        val obj = PinwheelEventChannelArgument("dd_form_create", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.DD_FORM_DOWNLOAD -> {
+        val obj = PinwheelEventChannelArgument("dd_form_download", null)
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.SCREEN_TRANSITION -> {
+        val obj = PinwheelEventChannelArgument("screen_transition", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.OTHER_EVENT -> {
+        val obj = PinwheelEventChannelArgument("other_event", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_SWITCH_SUCCESS -> {
+        val obj = PinwheelEventChannelArgument("bill_switch_success", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_SWITCH_FAILURE -> {
+        val obj = PinwheelEventChannelArgument("bill_switch_failure", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_REMOVED -> {
+        val obj = PinwheelEventChannelArgument("bill_removed", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_ADDED -> {
+        val obj = PinwheelEventChannelArgument("bill_added", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_EDITED -> {
+        val obj = PinwheelEventChannelArgument("bill_edited", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_MARKED_INACTIVE -> {
+        val obj = PinwheelEventChannelArgument("bill_marked_inactive", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_SWITCH_PLATFORMS_ADDED -> {
+        val obj = PinwheelEventChannelArgument("bill_switch_platforms_added", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_SWITCH_PLATFORMS_REMOVED -> {
+        val obj = PinwheelEventChannelArgument("bill_switch_platforms_removed", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_CANCEL_SUCCESS -> {
+        val obj = PinwheelEventChannelArgument("bill_cancel_success", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.BILL_CANCEL_FAILURE -> {
+        val obj = PinwheelEventChannelArgument("bill_cancel_failure", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.CALENDAR_SYNC -> {
+        val obj = PinwheelEventChannelArgument("calendar_sync", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.CUSTOMER_TERMS_ACCEPTED -> {
+        val obj = PinwheelEventChannelArgument("customer_terms_accepted", null)
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.USER_ACTIVATED -> {
+        val obj = PinwheelEventChannelArgument("user_activated", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.EXTERNAL_ACCOUNT_CONNECTED -> {
+        val obj = PinwheelEventChannelArgument("external_account_connected", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+      PinwheelEventType.MERCHANT_LOGIN_SUCCESS -> {
+        val obj = PinwheelEventChannelArgument("merchant_login_success", gson.toJson(payload))
+        argument = gson.toJson(obj)
+      }
+    }
 
     Handler(Looper.getMainLooper()).post {
       channel.invokeMethod("onEvent", argument)
